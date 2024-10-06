d2d047ab546a30a7f883cb50e0a27f834aa50b65
Unigwe <emmunigwe@gmail.com>
2024-10-07 00:47:02 +0100

    test

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """
- This module defines a function that generates pascal_triangle(n)
+ This module defines a function that generates pascal_triangle of n
 """
-
 
 def pascal_triangle(n):
     """
@@ -11,15 +10,16 @@ def pascal_triangle(n):
     Return a list of integers contain in the
     generated triangle
     """
+   
     if n <= 0:
         return []
 
-    triangle = []
+    a = []
 
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
+            row[j] = a[i - 1][j - 1] + a[i - 1][j]
+        a.append(row)
 
-    return triangle
+    return a

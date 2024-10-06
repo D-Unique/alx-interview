6915822d7d62766aa9d9844c2b7f6451509af489
Unigwe <emmunigwe@gmail.com>
2024-10-07 00:36:45 +0100

    fixed(0-pascal_triangle.py)

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
- This module defines a function that generates pascal_triangle of n
+ This module defines a function that generates pascal_triangle(n)
 """
 
 
 def pascal_triangle(n):
     """
-    returns a list of lists of integers representing the
-    Pascal's triangle of size n
+    Represent Pascal's triangle of size n.
+
+    Return a list of integers contain in the
+    generated triangle
     """
     if n <= 0:
         return []
 
-    a = []
+    triangle = []
 
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
-            row[j] = a[i - 1][j - 1] + a[i - 1][j]
-        a.append(row)
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
 
-    return a
+    return triangle

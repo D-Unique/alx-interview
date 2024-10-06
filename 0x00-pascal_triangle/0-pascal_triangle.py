7d1795a7cbe389950d7b8ed4e087a115a4f13bb5
Unigwe <emmunigwe@gmail.com>
2024-10-07 00:50:54 +0100

    test2

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,12 +14,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    a = []
-
+    a = [[] for i in range(n)]
     for i in range(n):
-        row = [1] * (i + 1)
-        for j in range(1, i):
-            row[j] = a[i - 1][j - 1] + a[i - 1][j]
-        a.append(row)
-
+        for j in range(i + 1):
+            if (j < i):
+                if (j == 0):
+                    a[i].append(1)
+                else:
+                    a[i].append(a[i-1][j] + a[i-1][j-1])
+            elif (j == i):
+                a[i].append(1)
     return a
